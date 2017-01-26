846922f38ca5967b9a7d5e8aa6aa0a14985c6fca
Fabio Ponciroli <fabio.ponciroli@net-a-porter.com>
2017-01-26 00:09:11 +0000

    resized buffers

--- blocking_backpressure.js
+++ blocking_backpressure.js
@@ -6,14 +6,14 @@ var httpServer = http.createServer(
     function handleRequest( request, response ) {
 
         // Read Buffer -> 10 bytes
-        var producer = fs.createReadStream( "./high_resolution_image.jpg", { highWaterMark: 10  } );
+        var producer = fs.createReadStream( "./resources/high_resolution_image.jpg", { highWaterMark: 10 * 1024  } );
 
         // Back-pressure effect:
         // - we are forcing the PassThrough buffer to fill up by:
         // -- forcing a small buffer
         // -- not piping it into anything else
         // - once the buffer is filled up, the stream create back-pressure and pauses the producer
-        producer.pipe( new stream.PassThrough( { highWaterMark: 1 } ));
+        producer.pipe( new stream.PassThrough( { highWaterMark: 10 * 1024 } ));
 
         producer.pipe( response );
     }
