3ba93a963b34a0e2758161c9747cef755d339a65
Fabio Ponciroli <fabio.ponciroli@net-a-porter.com>
2017-01-26 00:06:39 +0000

    Demonstrate backpressure effect

---
+++ blocking_backpressure.js
@@ -0,0 +1,24 @@
+var http = require( "http" );
+var fs = require( "fs" );
+var stream = require( "stream" );
+
+var httpServer = http.createServer(
+    function handleRequest( request, response ) {
+
+        // Read Buffer -> 10 bytes
+        var producer = fs.createReadStream( "./high_resolution_image.jpg", { highWaterMark: 10  } );
+
+        // Back-pressure effect:
+        // - we are forcing the PassThrough buffer to fill up by:
+        // -- forcing a small buffer
+        // -- not piping it into anything else
+        // - once the buffer is filled up, the stream create back-pressure and pauses the producer
+        producer.pipe( new stream.PassThrough( { highWaterMark: 1 } ));
+
+        producer.pipe( response );
+    }
+);
+
+httpServer.listen( 8080 );
+
+console.log( "Server running on port 8080" );
